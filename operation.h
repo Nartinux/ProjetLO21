984e6510b7be27a372713a750d13cb4a8f628021
Nartinux <martbird@hotmail.fr>
2016-05-30 16:07:10 +0200

    Ajout des operations MOD et DIV et ameliorations de l'interface graphique

--- operation.h
+++ operation.h
@@ -1,37 +1,4 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 
-#include <QString>
-#include <QTextStream>
-#include <QObject>
-#include <QDebug>
-
-
-class ToString
-{
-public:
-    QString toString()
-    {
-
-    }
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 #endif // OPERATION_H
